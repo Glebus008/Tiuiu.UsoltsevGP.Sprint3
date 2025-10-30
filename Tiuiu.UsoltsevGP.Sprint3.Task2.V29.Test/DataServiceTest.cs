f832b7717e9f64987edc682178edcdc061eaaf18
Глеб Усольцев <glebus008@gmail.com>
2025-10-31 02:15:54 +0500

    Task2

---
+++ Tiuiu.UsoltsevGP.Sprint3.Task2.V29.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tiuiu.UsoltsevGP.Sprint3.Task2.V29.Lib;
+namespace Tiuiu.UsoltsevGP.Sprint3.Task2.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+
+            double value = 0.5;
+            int startValue = 1;
+            int stopValue = 18;
+
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+
+            double wait = -0.186;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tiuiu.UsoltsevGP.Sprint3.Task2.V29/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tiuiu.UsoltsevGP.Sprint3.Task2.V29.Lib;
+namespace Tyuiu.UsoltsevGP.Sprint3.Task1.V24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Усольцев Г. П. | РППб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла do-while                                           *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнил: Усольцев Глеб Павлович | РППб-25-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл do...while                            *");
+            Console.WriteLine("* которая вычисляет сумму ряда по формуле, при a = 0,5                    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double value = 0.5;
+            int startValue = 1;
+            int stopValue = 18;
+
+            Console.WriteLine("Переменная X = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма рада = " + ds.GetSumSeries(value, startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
