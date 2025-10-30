29b905dfcd9efb93fbf4786d9687b1240c99a4d7
Глеб Усольцев <glebus008@gmail.com>
2025-10-31 03:32:01 +0500

    Task4

---
+++ Tiuiu.UsoltsevGP.Sprint3.Task4.V12/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tiuiu.UsoltsevGP.Sprint3.Task4.V12.Lib;
+namespace Tyuiu.UsoltsevGP.Sprint3.Task1.V24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Усольцев Г. П. | РППб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Использование операторов continue и break в циклах                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #12                                                             *");
+            Console.WriteLine("* Выполнил: Усольцев Глеб Павлович | РППб-25-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Вычислить значение функции y = x/(x + sin(x)) + 2.5                     *");
+            Console.WriteLine("* При х = 0 прервать цикл. Полученные значения перемножать.               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма рада = " + ds.Calculate(startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
