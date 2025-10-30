9a8eb21678d2268406a513c04315ac93d4a8659b
Глеб Усольцев <glebus008@gmail.com>
2025-10-31 01:05:59 +0500

    Add project files.

---
+++ Tiuiu.UsoltsevGP.Sprint3.Task0.V7.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tiuiu.UsoltsevGP.Sprint3.Task0.V7.Lib;
+namespace Tiuiu.UsoltsevGP.Sprint3.Task0.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+            
+            double value = 0.75;
+            int startValue = 1;
+            int stopValue = 20;
+
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+
+            double wait = 0.333;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tiuiu.UsoltsevGP.Sprint3.Task0.V7/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tiuiu.UsoltsevGP.Sprint3.Task0.V7.Lib;
+namespace Tyuiu.UsoltsevGP.Sprint3.Task0.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Усольцев Г. П. | РППб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла for                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Усольцев Глеб Павлович | РППб-25-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл for                                   *");
+            Console.WriteLine("* которая вычисляет сумму ряда по формуле, при a = 0,75                   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            
+            double value = 0.75;
+            int startValue = 1;
+            int stopValue = 20;
+
+            Console.WriteLine("Переменная X = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма рада = " + ds.GetSumSeries(value, startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
