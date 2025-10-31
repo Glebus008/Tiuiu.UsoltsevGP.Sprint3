020e71c618bed43b535dc7af87841270d25b49dc
Глеб Усольцев <glebus008@gmail.com>
2025-10-31 06:08:57 +0500

    Task7

---
+++ Tiuiu.UsoltsevGP.Sprint3.Task7.V4/Program.cs
@@ -0,0 +1,53 @@
+﻿using Tiuiu.UsoltsevGP.Sprint3.Task7.V4.Lib;
+namespace Tyuiu.UsoltsevGP.Sprint3.Task7.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Усольцев Г. П. | РППб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Усольцев Глеб Павлович | РППб-25-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая выводит таблицу значений функции            *");
+            Console.WriteLine("* числовому отрезку [10, 19] количество всех делителей больше 10          *");
+            Console.WriteLine("* на диапазоне [-5;5] c шагом 1                                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+            double[] valueArray;
+            valueArray = new double[len];
+
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("+----------+-----------+");
+            Console.WriteLine("|     X    |    f(x)   |");
+            Console.WriteLine("+----------+-----------+");
+            for (int i = 0; i < len; i++)
+            {
+                Console.WriteLine("|{0,5:d}     |  {1,6:f2}   |", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+----------+-----------+");
+            Console.ReadKey();
+        }
+    }
+}
